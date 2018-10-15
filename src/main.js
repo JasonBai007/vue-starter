c19d0abb8db3d964a491582b50d14ae103584107
JasonBai007 <byygkcg@163.com>
2018-10-15 15:22:53 +0800

    引入饿了么组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入AXIOS
+import axios from 'axios'
+// 完整引入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
