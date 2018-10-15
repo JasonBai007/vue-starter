cf1d17dfc636d327915537edf4ee39b9195e48e3
JasonBai007 <byygkcg@163.com>
2018-10-15 13:42:17 +0800

    修改webpack配置

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ FE：鲁班七号、铠、蔡文姬
 BE：大乔、老夫子
 
 ## Useful links
+vue/cli：[https://cli.vuejs.org/zh/guide/webpack.html](https://cli.vuejs.org/zh/guide/webpack.html)  
 产品文档：  
 接口文档：  
 仓库地址：
@@ -49,3 +50,5 @@ Ctrl + C
 ## Notes
 1. 使用前，确保已安装了最新版的第三方组件库。
 2. 使用前，请先仔细阅读Readme文档并完善上面的必要信息。
+3. 如需修改webpack配置，请修改vue.config.js文件。
+4. 查看并输出webpack配置，在项目根目录执行：vue inspect > output.js
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
-const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+// Config Reference: https://cli.vuejs.org/zh/config/
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new OpenBrowserPlugin({ url: 'http://localhost:8080' })
-    ]
+  productionSourceMap: false,
+  devServer: {
+    port: 8080, // 配置端口号
+    host: 'localhost',
+    https: false,
+    open: true, // 配置自动启动浏览器
+    // 配置代理
+    proxy: {
+      '/api/v1': {
+        target: 'http://xxxx:8888/',
+        changeOrigin: true
+      }
+    }
   }
 }
