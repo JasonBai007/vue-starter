46eae3ae2e944fcdcb36725db6d32d0addc21d04
JasonBai007 <byygkcg@163.com>
2018-10-17 09:45:26 +0800

    实现页面title自动化修改

--- src/router.js
+++ src/router.js
@@ -4,20 +4,35 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
+const router = new Router({
+  routes: [{
       path: '/',
       name: 'home',
+      meta: {
+        title: '首页'
+      },
       component: Home
     },
     {
       path: '/about',
       name: 'about',
+      meta: {
+        title: '关于我'
+      },
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import ('./views/About.vue')
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // 自动化修改页面标签的 title
+  document.title = to.meta.title
+    // 确保要调用 next 方法，否则钩子就不会被 resolved
+  next()
+})
+
+export default router
