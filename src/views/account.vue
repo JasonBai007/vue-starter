818730a3de605cbe9a9ad49fe282cc2da6483626
JasonBai007 <byygkcg@163.com>
2019-12-23 11:25:55 +0800

    添加筛选条

--- src/views/account.vue
+++ src/views/account.vue
@@ -1,7 +1,40 @@
 <template>
-  <div class="wrap">
+  <div class="account-wrap">
     <!-- 条件筛选区 -->
-    <el-card class="filter-bar">asdfa</el-card>
+    <el-card class="filter-bar">
+      <el-row type="flex" align="bottom" :gutter="20">
+        <el-col :span="18">
+          <el-form label-position="top" label-width="80px" :model="formData">
+            <el-row :gutter="20">
+              <el-col :span="8">
+                <el-form-item label="姓名">
+                  <el-input v-model="formData.name" placeholder="请输入姓名"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="类型">
+                  <el-select v-model="formData.type" placeholder="请选择类型"  style="width:100%">
+                    <el-option label="射手" value="射手"></el-option>
+                    <el-option label="法师" value="法师"></el-option>
+                    <el-option label="辅助" value="辅助"></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="住址">
+                  <el-input v-model="formData.type" placeholder="请输入住址"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+        </el-col>
+        <el-col :span="6">
+          <el-button>重置</el-button>
+          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+          <el-button type="primary" icon="el-icon-plus" style="float:right">新建</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
     <!-- 表格区 -->
     <el-card>
       <el-table :data="tableData" stripe>
@@ -33,6 +66,11 @@ export default {
   components: {},
   data() {
     return {
+      formData: {
+        name: "",
+        region: "",
+        type: ""
+      },
       tableData: []
     };
   },
@@ -52,6 +90,12 @@ export default {
 <style lang="scss" scoped>
 .filter-bar {
   margin-bottom: 20px;
+}
+.el-form-item {
+  margin-bottom: 0 !important;
+}
+.el-form-item__label {
+  margin-top: -15px !important;
 }
 .btn {
   display: inline-block;
