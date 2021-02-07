703672b002d1059caeda44160dc1b08aba420692
JasonBai007 <byygkcg@163.com>
2021-02-07 10:47:16 +0800

    完善ws模块

--- src/ws.js
+++ src/ws.js
@@ -2,3 +2,8 @@ let ws = new WebSocket('ws://123.207.136.134:9010/ajaxchattest')
 ws.onopen = e => {
     console.info('websocket connected')
 }
+ws.onclose = e => {
+    console.info('websocket closed')
+}
+
+export default ws;
