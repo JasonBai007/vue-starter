e8b4317a86536b5fa9bdcfabdfdab00868b56613
JasonBai007 <byygkcg@163.com>
2021-07-31 15:12:08 +0800

    生产环境去除控制台打印

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
+let plugins = []
+if (process.env.NODE_ENV === 'production') {
+  plugins.push("transform-remove-console")
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins
 }
