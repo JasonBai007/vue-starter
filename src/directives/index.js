65553cb850f7653d9fdc1010312f6e321591ca6a
JasonBai007 <byygkcg@163.com>
2021-11-11 15:22:23 +0800

    更新指令

--- src/directives/index.js
+++ src/directives/index.js
@@ -47,10 +47,13 @@ Vue.directive('count', {
   }
 });
 
-// 拖拽功能
+// 拖拽功能（PC端）
 Vue.directive('drag', {
   inserted(el) {
     el.onmousedown = (e) => {
+      // 拖拽开始的时候，将当前浮动框层级提升到最高
+      // document.querySelectorAll('.block-wrap').forEach(node => node.style.zIndex = 2);
+      // e.target.parentNode.style.zIndex = 3;
       let innerX = e.clientX - el.parentNode.offsetLeft;
       let innerY = e.clientY - el.parentNode.offsetTop;
       document.onmousemove = (e) => {
@@ -63,6 +66,41 @@ Vue.directive('drag', {
       }
       // 防止黏连
       return false;
+    }
+  }
+})
+
+// 移动版drag指令
+Vue.directive('drag-m', {
+  inserted(el) {
+    el.ontouchstart = (e) => {
+      e.preventDefault()
+      let innerX = e.targetTouches[0].clientX - el.parentNode.offsetLeft;
+      let innerY = e.targetTouches[0].clientY - el.parentNode.offsetTop;
+      document.ontouchmove = (e) => {
+        // 计算出右边界
+        let rightLine = e.targetTouches[0].clientX - innerX + el.parentNode.offsetWidth
+        if (rightLine <= el.parentNode.offsetWidth) {
+          // 右边界 <= 浮动框的宽度
+          el.parentNode.style.left = '0px';
+        } else if (rightLine > el.parentNode.offsetWidth && rightLine < document.documentElement.clientWidth) {
+          // 浮动框的宽度 <= 右边界 <= 页面宽度
+          el.parentNode.style.left = e.targetTouches[0].clientX - innerX + 'px';
+        } else {
+          // 右边界 > 页面宽度
+          el.parentNode.style.left = document.documentElement.clientWidth - el.parentNode.offsetWidth + 'px';
+        }
+        // 距离顶部距离 > 0
+        if (e.targetTouches[0].clientY - innerY > 0) {
+          el.parentNode.style.top = e.targetTouches[0].clientY - innerY + 'px';
+        } else {
+          el.parentNode.style.top = '0px';
+        }
+      }
+      document.ontouchend = () => {
+        document.ontouchmove = null;
+        document.ontouchend = null;
+      }
     }
   }
 })
