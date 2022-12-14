98fad9c060f9b9e40fc217c9ff1625d89f04b1af
JasonBai007 <byygkcg@163.com>
2022-12-14 14:07:36 +0800

    新增设备

---
+++ src/components/Lift.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="lift">
+    <div class="left" :style="{ top: 421 - options.left.height + 'px' }">
+      <!-- 后门 -->
+      <img src="../assets/img/components/lift/leftBack.png" :class="{ leftBack: true, openLeftDoor: options.left.backStatus, closeLeftDoor: !options.left.backStatus }" />
+      <!-- 笼子 -->
+      <img src="../assets/img/components/lift/leftCage.png" class="leftCage" />
+      <!-- 前门 -->
+      <img src="../assets/img/components/lift/leftFront.png" :class="{ leftFront: true, openLeftDoor: options.left.frontStatus, closeLeftDoor: !options.left.frontStatus }" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "lift",
+  props: ["options"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.lift {
+  position: relative;
+  width: 500px;
+  height: 540px;
+  background: url(../assets/img/components/lift/bg.png);
+  background-repeat: no-repeat;
+  background-position: center bottom;
+}
+.left {
+  position: absolute;
+  left: 133px;
+  width: 97px;
+  height: 106px;
+}
+.left img {
+  position: absolute;
+}
+.leftBack,
+.leftFront {
+  width: 97px;
+  height: 106px;
+}
+.leftCage {
+  top: -31px;
+}
+
+.openLeftDoor {
+  animation: openDoor 1s ease-out forwards;
+}
+.closeLeftDoor {
+  animation: closeDoor 1s ease-out forwards;
+}
+
+@keyframes openDoor {
+  0% {
+    top: 0;
+    opacity: 1;
+  }
+  100% {
+    top: -137px;
+    opacity: 0;
+  }
+}
+
+@keyframes closeDoor {
+  0% {
+    top: -137px;
+    opacity: 0;
+  }
+  100% {
+    top: 0;
+    opacity: 1;
+  }
+}
+</style>
--- src/views/tower.vue
+++ src/views/tower.vue
@@ -1,29 +1,54 @@
 <template>
-  <div class="tower-wrap">
-    <el-card>
-      <TowerCrane :options="options"></TowerCrane>
-    </el-card>
-    <el-card class="controls">
-      <h3>Left</h3>
-      <el-slider v-model="options.left" :min="150" :max="430"></el-slider>
-      <h3>Height</h3>
-      <el-slider v-model="options.height" :min="0" :max="300"></el-slider>
-    </el-card>
+  <div>
+    <div class="tower-wrap">
+      <el-card>
+        <TowerCrane :options="options"></TowerCrane>
+      </el-card>
+      <el-card class="controls">
+        <h3>Left</h3>
+        <el-slider v-model="options.left" :min="150" :max="430"></el-slider>
+        <h3>Height</h3>
+        <el-slider v-model="options.height" :min="0" :max="300"></el-slider>
+      </el-card>
+    </div>
+    <div class="tower-wrap">
+      <el-card>
+        <Lift :options="optionsLift"></Lift>
+      </el-card>
+      <el-card class="controls">
+        <h3>Left</h3>
+        <h3>Height</h3>
+        <el-slider v-model="optionsLift.left.height" :min="0" :max="421"></el-slider>
+        <h3>Front Door</h3>
+        <el-switch v-model="optionsLift.left.frontStatus" active-color="#13ce66" inactive-color="#ff4949"> </el-switch>
+        <h3>Back Door</h3>
+        <el-switch v-model="optionsLift.left.backStatus" active-color="#13ce66" inactive-color="#ff4949"> </el-switch>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
 import TowerCrane from "@/components/TowerCrane";
+import Lift from "@/components/Lift";
 export default {
   name: "tower",
   components: {
     TowerCrane,
+    Lift,
   },
   data() {
     return {
       options: {
         left: 200, // 150-430
         height: 200, // 0-300
+      },
+      optionsLift: {
+        left: {
+          height: 200, // 0-400,
+          frontStatus: true,
+          backStatus: true,
+        },
       },
     };
   },
@@ -35,6 +60,7 @@ export default {
 .tower-wrap {
   display: flex;
   gap: 10px;
+  margin-bottom: 10px;
   .controls {
     width: auto;
     flex-grow: 1;
